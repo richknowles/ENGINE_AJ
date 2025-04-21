27d141f053df3a0bfa15b472d99c4f8aeb8ed46e
Rich Knowles <rich@itwerks.net>
2025-04-20 19:09:20 -0500

    Delete app directory

--- app/aj_driver.py
+++
@@ -1,9 +0,0 @@
-from undetected_chromedriver import Chrome, ChromeOptions
-
-def get_driver():
-    options = ChromeOptions()
-    options.add_argument('--user-data-dir=./user_data')
-    options.add_argument('--profile-directory=Default')
-    driver = Chrome(options=options)
-    driver.get("https://web.whatsapp.com")
-    return driver
